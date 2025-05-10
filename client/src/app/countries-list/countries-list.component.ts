6c9c1489a5621acd4f876bae5a0521429090cc61
Celumusa Duma <celumusalwandle@yahoo.com>
2025-05-10 23:11:02 +0200

    add pagination

--- client/src/app/countries-list/countries-list.component.ts
+++ client/src/app/countries-list/countries-list.component.ts
@@ -11,12 +11,45 @@ import { Country, CountryService } from '../services/country.service';
 })
 export class CountriesListComponent {
   countries: Country[] = [];
+  paginatedCountries: Country[] = [];
+  currentPage  = 1;
+  pageSize = 12;
 
   constructor(private countryService: CountryService) {}
 
   ngOnInit(): void {
     this.countryService.getAllCountries().subscribe(data => {
       this.countries = data;
+      this.updatePagination();
     });
+  }
+
+    updatePagination(): void {
+    const start = (this.currentPage - 1) * this.pageSize;
+    const end = start + this.pageSize;
+    this.paginatedCountries = this.countries.slice(start, end);
+  }
+
+  nextPage(): void {
+    if ((this.currentPage * this.pageSize) < this.countries.length) {
+      this.currentPage++;
+      this.updatePagination();
+    }
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePagination();
+    }
+  }
+
+  goToPage(page: number): void {
+    this.currentPage = page;
+    this.updatePagination();
+  }
+
+  get totalPages(): number[] {
+    return Array.from({ length: Math.ceil(this.countries.length / this.pageSize) }, (_, i) => i + 1);
   }
 }
